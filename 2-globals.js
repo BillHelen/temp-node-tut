9117ac6266c43aed85557d92cdb370bccf45db05
George <gmorris2@twc.com>
2021-12-18 18:55:25 -0500

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,15 @@
+// GLOBALS - NO WINDOW 1111
+
+// __dirname  - path to current directory
+// __filename - file name
+// require   - function to use modules (CommonJS)
+// module    - info about current module (file)
+// process   - info about env where the program is being executed
+
+//console.log(__dirname);
+console.log(__filename);
+setInterval(() =>{
+    console.log('hello world')
+}, 1000)
+
+// https://www.youtube.com/watch?v=TNV0_7QRDwY 29:41
